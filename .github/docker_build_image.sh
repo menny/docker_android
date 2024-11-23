ea6403343e525e01c11888ed6e49a7e704253cd5
Menny Even Danan <menny@evendanan.net>
2024-11-23 08:33:07 -0500

    Showing squash stats

--- .github/docker_build_image.sh
+++ .github/docker_build_image.sh
@@ -3,6 +3,7 @@ set -e
 
 docker build ${DOCKER_FILE_PATH} --build-arg IMAGE_VERSION="${IMAGE_VERSION}" --build-arg NDK_VERSION="${NDK_VERSION}" --build-arg BAZELISK_VERSION="${BAZELISK_VERSION}" --compress -t "menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw"
 
+SQUASH_MSG=""
 if [[ "SQUASH" == "${SQUASH_IMAGE}" ]]; then
     # Squashing
     pip install docker-squash
@@ -16,6 +17,7 @@ if [[ "SQUASH" == "${SQUASH_IMAGE}" ]]; then
     echo "******"
 
     COMMITS_COUNT="$(docker history menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw | grep 'buildkit.dockerfile' | wc -l)"
+    IMAGE_SIZE_RAW=$(docker inspect -f "{{ .Size }}" "menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw")
     echo "** docker-squash $COMMITS_COUNT layers"
     docker-squash -f "$COMMITS_COUNT" -t "menny/${IMAGE_NAME}:${IMAGE_VERSION}" menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw
     echo "******"
@@ -23,14 +25,16 @@ if [[ "SQUASH" == "${SQUASH_IMAGE}" ]]; then
     echo "** Docker images on machine after squash:"
     docker images
     echo "******"
+    SQUASH_MSG=" Layers ${COMMITS_COUNT}. Size before squashing: ${IMAGE_SIZE_RAW}" 
 else
     echo "Skipping squashing"
+    SQUASH_MSG=" No squash."
     docker tag "menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw" "menny/${IMAGE_NAME}:${IMAGE_VERSION}"
 fi
 
 IMAGE_SIZE_RAW=$(docker inspect -f "{{ .Size }}" menny/${IMAGE_NAME}:${IMAGE_VERSION})
 IMAGE_SIZE=$(echo $IMAGE_SIZE_RAW | numfmt --to=si)
-SIZE_MESSAGE="Image size for \`menny/${IMAGE_NAME}:${IMAGE_VERSION}\` is ${IMAGE_SIZE} (or $IMAGE_SIZE_RAW bytes)."
+SIZE_MESSAGE="Image size for \`menny/${IMAGE_NAME}:${IMAGE_VERSION}\` is ${IMAGE_SIZE} (or $IMAGE_SIZE_RAW bytes).${SQUASH_MSG}"
 echo "${SIZE_MESSAGE}"
 
 if [[ -n "$GITHUB_COMMENT_URL" ]]; then
