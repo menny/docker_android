e551e8280e290c680ad3462947c5afd7d62ec9c9
Menny Even Danan <menny@evendanan.net>
2024-11-30 23:01:00 -0500

    Read version env variables from file

--- .github/docker_build_image.sh
+++ .github/docker_build_image.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-source "$(dirname -- "${BASH_SOURCE[0]}")/shared_tools_versions.sh"
-
 docker build ${DOCKER_FILE_PATH} --build-arg IMAGE_VERSION="${IMAGE_VERSION}" --build-arg NDK_VERSION="${NDK_VERSION}" --build-arg BAZELISK_VERSION="${BAZELISK_VERSION}" --compress -t "menny/${IMAGE_NAME}:${IMAGE_VERSION}-raw"
 
 SQUASH_MSG=""
