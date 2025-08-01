7dae7d6bf6f7ac2acfad6f66108c482f5fce022b
Menny Even Danan <menny@evendanan.net>
2025-08-01 16:45:22 -0400

    Fixes to make this actually work

--- android_dev/entrypoint.sh
+++ android_dev/entrypoint.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-# Set default values for git variables
-GIT_REPO=${git_repo:-"AnySoftKeyboard/AnySoftKeyboard"}
-GIT_BRACH=${git_brach:-"main"}
-GIT_PROVIDER=${git_provider:-"github.com"}
-
 # Clone the repository
-git clone --branch ${GIT_BRACH} https://${GIT_PROVIDER}/${GIT_REPO}
+git clone --branch "${GIT_BRANCH:-"main"}" "https://${GIT_PROVIDER:-"github.com"}/${GIT_REPO:-"AnySoftKeyboard/AnySoftKeyboard"}"
 
 # Start an interactive zsh shell
 exec zsh
