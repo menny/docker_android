773a0853f14aea658ab6b89128ea0ee2aa6a1059
Menny Even Danan <menny@evendanan.net>
2025-08-03 07:56:31 -0400

    starting tmux not as exec

--- android_dev/start_tmux_dev_session.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script checks for a tmux session named "dev-session".
-# If the session exists, it attaches to it.
-# If it does not exist, it creates a new session with that name.
-
-tmux attach-session -t dev-session 2>/dev/null || tmux new-session -s dev-session
